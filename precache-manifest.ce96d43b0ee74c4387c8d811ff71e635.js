e64365669b08fddd38c38457538282148a825c36
dangle <dangle@orange>
2020-08-19 09:18:55 -0400

    Updates

---
+++ precache-manifest.ce96d43b0ee74c4387c8d811ff71e635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff489985edd08975dfe9dcd7ae6d680",
+    "url": "/react_git/index.html"
+  },
+  {
+    "revision": "29d5fce698ef43fe9b82",
+    "url": "/react_git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7c403f977da9dc43e4a",
+    "url": "/react_git/static/js/2.394634c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_git/static/js/2.394634c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d5fce698ef43fe9b82",
+    "url": "/react_git/static/js/main.9f4736e6.chunk.js"
+  },
+  {
+    "revision": "a191e8a66cb88e740536",
+    "url": "/react_git/static/js/runtime-main.233d61c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_git/static/media/logo.5d5d9eef.svg"
+  }
+]);
